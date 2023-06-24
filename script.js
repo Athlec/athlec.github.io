2f75cab2a4a4688084506eef2c42c8f30377eb5d
Leonardo Delejon <137613011+Athlec@users.noreply.github.com>
2023-06-24 16:29:00 +0200

    Revert "Merge pull request #1 from LadyLing/grayout"
    This reverts commit 0725f82452937d0b9da144225c08fdf33e1aea6d, reversing
    changes made to d0d5039b04a4a0161bb0cbaf8846b2efbab10d69.

--- script.js
+++ script.js
@@ -1,40 +1,10 @@
-var isDisappearingMode = true;
-
-function toggleImage(image) {
-  if (isDisappearingMode) {
-    image.style.display = "none";
-  } else {
-    image.classList.toggle("grayscale");
-  }
+function hideImage(image) {
+  image.style.display = "none";
 }
 
 function resetImages() {
   var images = document.getElementsByTagName("img");
   for (var i = 0; i < images.length; i++) {
     images[i].style.display = "inline";
-    images[i].classList.remove("grayscale");
   }
 }
-
-function toggleMode() {
-  isDisappearingMode = !isDisappearingMode;
-}
-
-function openFileInput() {
-  document.getElementById("file-input").click();
-}
-
-document.getElementById("file-input").addEventListener("change", function(event) {
-  var fileList = event.target.files;
-  var imageContainer = document.getElementById("image-containert1");
-
-  for (var i = 0; i < fileList.length; i++) {
-    var file = fileList[i];
-    var image = document.createElement("img");
-    image.src = URL.createObjectURL(file);
-    image.onclick = function() {
-      toggleImage(this);
-    };
-    imageContainer.appendChild(image);
-  }
-});
