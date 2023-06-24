ec6cf02bda257fbabdc88ae0ca7213a08ad081c6
SaberPT <wowfreezer64@gmail.com>
2023-06-24 21:20:25 +0200

    add grayout mode and move style from .html to style.css

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
+var isDisappearingMode = true;
+
 function hideImage(image) {
-  image.style.display = "none";
+  if (isDisappearingMode) {
+    image.style.display = "none";
+  } else {
+    image.classList.toggle("grayscale");
+  }
 }
 
 function resetImages() {
   var images = document.getElementsByTagName("img");
   for (var i = 0; i < images.length; i++) {
     images[i].style.display = "inline";
+    images[i].classList.remove("grayscale");
   }
 }
+
+function toggleMode() {
+  isDisappearingMode = !isDisappearingMode;
+}
+
+function openFileInput() {
+  document.getElementById("file-input").click();
+}
+
+document.getElementById("file-input").addEventListener("change", function(event) {
+  var fileList = event.target.files;
+  var imageContainer = document.getElementById("image-containert6");
+
+  for (var i = 0; i < fileList.length; i++) {
+    var file = fileList[i];
+    var image = document.createElement("img");
+    image.src = URL.createObjectURL(file);
+    image.onclick = function() {
+      toggleImage(this);
+    };
+    imageContainer.appendChild(image);
+  }
+});
