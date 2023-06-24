f98a5329a4df7d55d2fda98a8bbfc0c75dedc6a4
SaberPT <wowfreezer64@gmail.com>
2023-06-24 13:50:45 +0200

    grayout toggle mode

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function hideImage(image) {
-  image.style.display = "none";
+var isDisappearingMode = true;
+
+function toggleImage(image) {
+  if (isDisappearingMode) {
+    image.style.display = "none";
+  } else {
+    image.classList.toggle("grayscale");
+  }
 }
 
 function resetImages() {
   var images = document.getElementsByTagName("img");
   for (var i = 0; i < images.length; i++) {
     images[i].style.display = "inline";
+    images[i].classList.remove("grayscale");
   }
+}
+
+function toggleMode() {
+  isDisappearingMode = !isDisappearingMode;
 }
